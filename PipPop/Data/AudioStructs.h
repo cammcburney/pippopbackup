cae45e7a6ebb0ff7b5bb832f72de72d84f186a5b
cammcburney <cammcburney95@gmail.com>
2026-04-30 20:45:00 +0100

    audio struct for playback

---
+++ PipPop/Data/AudioStructs.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AudioStructs.generated.h"
+
+USTRUCT(BlueprintType)
+struct PIPPOP_API FSoundEffect
+{
+	GENERATED_USTRUCT_BODY()
+	
+	UPROPERTY(EditAnywhere, Category="Audio")
+	TObjectPtr<USoundBase> Sound = nullptr;
+
+	UPROPERTY(EditAnywhere, Category="Audio")
+	float Volume = 1.f;
+	
+	UPROPERTY(EditAnywhere, Category="Audio")
+	float Pitch = 1.f;
+
+	UPROPERTY(EditAnywhere, Category="Audio")
+	float StartTime = 0.f;
+};
