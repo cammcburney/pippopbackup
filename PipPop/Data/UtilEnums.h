9005f88ce5979bd062c44a8356b19a4f780c1636
cammcburney <cammcburney95@gmail.com>
2026-05-14 07:47:54 +0100

    add weapon and utility enums to start structuring player inventory

---
+++ PipPop/Data/UtilEnums.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UtilEnums.generated.h"
+
+UENUM()
+enum class EDirections : uint8
+{
+	UP = 0 UMETA(DisplayName = "UP"),
+	DOWN = 1 UMETA(DisplayName = "DOWN"),
+	LEFT = 2 UMETA(DisplayName = "LEFT"),
+	RIGHT = 3 UMETA(DisplayName = "RIGHT"),
+};
---
+++ PipPop/Data/WeaponStructs.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "LevelStructs.generated.h"
+#include "Character/Weapons/BaseWeapon.h"
+
+USTRUCT(BlueprintType)
+struct PIPPOP_API FLevelNames: public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+	
+	UPROPERTY()
+	TObjectPtr<ABaseWeapon> PrimaryWeapon;
+
+	UPROPERTY()
+	TObjectPtr<ABaseWeapon> SecondaryWeapon;
+
+	UPROPERTY()
+	TObjectPtr<ABaseWeapon> SpecialWeapon;
+
+	UPROPERTY()
+	TObjectPtr<ABaseWeapon> GrenadeWeapon;
+};
+
+UENUM()
+enum class EWeaponTypes : uint8
+{
+	CORE = 0 UMETA(DisplayName = "Core"),
+	SPECIAL = 1 UMETA(DisplayName = "Special"),
+	GRENADE = 2 UMETA(DisplayName = "Grenade")
+};
